3b2a3d7811f03aa055f213c6e8fbaeab49d6e261
Pherlar <christopherlarmour@gmail.com>
2019-01-28 15:28:51 +1100

    Add more toppings, add input checkers to quantity

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -9,12 +9,16 @@ package com.example.android.justjava;
  **/
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.text.NumberFormat;
 
 /**
@@ -22,6 +26,9 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
     int quantity = 0;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,13 +41,23 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price = calculatePrice();
-        // check to see if order button is clicked
+
+        //get name from input box
+        EditText nameEditText = findViewById(R.id.nameEditTextView);
+        String customerName = nameEditText.getText().toString();
+
+        // check to see if whipped cream checkbox is clicked
         CheckBox whippedCreamCheckbox = findViewById(R.id.checkBox1);
         boolean hasWhippedCreamBoolean = whippedCreamCheckbox.isChecked();
         Log.i("MainActivity", "has whipped cream "+ hasWhippedCreamBoolean);
 
-        displayMessage(createOrderSummary(quantity,price,hasWhippedCreamBoolean));
+        // check to see if chocolate checkbox is clicked
+        CheckBox chocolateCheckbox = findViewById(R.id.checkBox2);
+        boolean hasChocolateBoolean = chocolateCheckbox.isChecked();
+        Log.i("MainActivity", "has chocolate "+ hasChocolateBoolean);
+
+        int price = calculatePrice(hasWhippedCreamBoolean, hasChocolateBoolean);
+        displayMessage(createOrderSummary(customerName,quantity,price,hasWhippedCreamBoolean,hasChocolateBoolean));
     }
 
     /**
@@ -55,7 +72,15 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the increment button is clicked.
      */
     public void incrementQuantity(View view) {
-        quantity =  quantity + 1;
+        if (quantity <10)
+        {
+            quantity = quantity + 1;
+        }
+        else
+        {
+            Toast.makeText(this, "Max Coffees Reached!", Toast.LENGTH_SHORT).show();
+        }
+
         displayQuantity (quantity);
 
     }
@@ -65,8 +90,18 @@ public class MainActivity extends AppCompatActivity {
      */
 
     public void decrementQuantity(View view) {
-        quantity = quantity - 1;
-        displayQuantity (quantity );
+
+        if (quantity >0)
+        {
+            quantity = quantity - 1;
+        }
+        else{
+            Toast.makeText(this, "Min Coffees Reached!", Toast.LENGTH_SHORT).show();
+
+        }
+
+        displayQuantity (quantity);
+
     }
 
 
@@ -82,14 +117,24 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method calculates the total price
      */
-    private int calculatePrice (){
-        return quantity * 5;
-
+    private int calculatePrice (boolean hasWhippedCreamBoolean, boolean hasChocolateBoolean ){
+        int price = quantity*5;
+        if (hasWhippedCreamBoolean)
+        {
+            price += (1 * quantity);
+        }
+        if (hasChocolateBoolean)
+        {
+            price += (2 * quantity);
+        }
+    return price;
     }
 
-    private String createOrderSummary (int quantity, int price, boolean hasWhippedCreamBoolean){
-        String priceMessage = "Name:  Chris";
+    private String createOrderSummary
+            (String customerName, int quantity, int price, boolean hasWhippedCreamBoolean, boolean hasChocolateBoolean){
+        String priceMessage = "Name:  " + customerName;
         priceMessage += "\nAdd whipped Cream? " + hasWhippedCreamBoolean;
+        priceMessage += "\nAdd Chocolate? " + hasChocolateBoolean;
         priceMessage += "\nQuantity: " + quantity;
         priceMessage += "\nTotal: $" + price;
         priceMessage += "\nThank You!";
